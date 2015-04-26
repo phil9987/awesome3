97c225e74c3dea1c5b18d6eb06011af853518645
Tim <tim@voodle.de>
2015-04-26 21:58:03 +0200

    small changes (mostly n_iter to 500)

--- Julia_src/shallownetwork.jl
+++ Julia_src/shallownetwork.jl
@@ -19,22 +19,22 @@ lr_policy = LRPolicy.Staged(
   (5000, LRPolicy.Fixed(0.00001)),
 )
 #lr_policy=LRPolicy.Inv(0.01,0.0001,0.75)
-params = SolverParameters(max_iter=5000, regu_coef=0.0004,
+params = SolverParameters(max_iter=1000, regu_coef=0.0004,
 	mom_policy=MomPolicy.Fixed(0.9),
 	lr_policy=lr_policy,
 	load_from=exp_dir)
 solver=SGD(params)
 
-setup_coffee_lounge(solver, save_into="$exp_dir/output.jld", every_n_iter=250)
+setup_coffee_lounge(solver, save_into="$exp_dir/output.jld", every_n_iter=500)
 
-add_coffee_break(solver, TrainingSummary(), every_n_iter=250)
-add_coffee_break(solver, Snapshot(exp_dir), every_n_iter=5000)
+add_coffee_break(solver, TrainingSummary(), every_n_iter=500)
+add_coffee_break(solver, Snapshot(exp_dir), every_n_iter=500)
 
 data_train_test = AsyncHDF5DataLayer(name="data-test",source="train_test_data.txt",batch_size=100)
 accuracy_test = AccuracyLayer(name="accuracy-test",bottoms=[:pred, :label])
 net_test = Net("images-test", backend, [data_train_test, common_layers..., accuracy_test])
 
-add_coffee_break(solver, ValidationPerformance(net_test), every_n_iter=100)
+add_coffee_break(solver, ValidationPerformance(net_test), every_n_iter=500)
 
 solve(solver, net)
 
@@ -44,12 +44,11 @@ net_val = Net("images-val", backend, [data_val, common_layers..., output_val])
 
 forward(net_val)
 
-data_test = AsyncHDF5DataLayer(name="testdata",source="test_data.txt",batch_size=10000,tops=[:data])
+data_test = AsyncHDF5DataLayer(name="data-test",source="test_data.txt",batch_size=10000,tops=[:data])
 output_test = HDF5OutputLayer(name="output-test",filename="test_output.h5",force_overwrite=true,bottoms=[:pred],datasets=[:label])
 net_testdata = Net("images-test",backend,[data_test, common_layers..., output_test])
 
 forward(net_testdata)
-
 
 destroy(net)
 destroy(net_test)
