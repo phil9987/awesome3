bef4dc4561782ba50b568eaf22f5d4a02b64f57b
Tim <tim@voodle.de>
2015-04-26 13:41:41 +0200

    only one layer

--- Julia_src/shallownetwork.jl
+++ Julia_src/shallownetwork.jl
@@ -3,15 +3,18 @@ using Mocha
 backend = CPUBackend()
 init(backend)
 
-data = AsyncHDF5DataLayer(name="data", source="train_data.txt", batch_size=250, shuffle=true)
+data = AsyncHDF5DataLayer(name="data", source="train_data.txt", batch_size=128, shuffle=true)
+
+common_layers = [
+# 								 InnerProductLayer(name="ip1",output_dim=2048,tops=[:ip1],bottoms=[:data],neuron=Neurons.ReLU()),
+								 InnerProductLayer(name="ip2",output_dim=10,tops=[:pred],bottoms=[:data])]
+
 loss = SoftmaxLossLayer(name="loss",bottoms=[:pred,:label])
 
-common_layers = [InnerProductLayer(name="ip1",output_dim=10,tops=[:ip1],bottoms=[:data]),
-								 IdentityLayer(name="pred",tops=[:pred],bottoms=[:ip1])]
 net = Net("images", backend, [data, common_layers..., loss])
 
 exp_dir= "snapshots"
-params = SolverParameters(max_iter=10000, regu_coef=0.0005,
+params = SolverParameters(max_iter=5000, regu_coef=0.0005,
 	mom_policy=MomPolicy.Fixed(0.9),
 	lr_policy=LRPolicy.Inv(0.01,0.0001,0.75),
 	load_from=exp_dir)
@@ -19,21 +22,18 @@ solver=SGD(params)
 
 setup_coffee_lounge(solver, save_into="$exp_dir/output.jld", every_n_iter=1000)
 
-# report training progress every 100 iterations
-add_coffee_break(solver,TrainingSummary(),every_n_iter=250)
+add_coffee_break(solver, TrainingSummary(), every_n_iter=250)
+add_coffee_break(solver, Snapshot(exp_dir), every_n_iter=1000)
 
-# save snapshots every 1000 iterations
-# add_coffee_break(solver, Snapshot(exp_dir), every_n_iter=1000)
-
-# show performance on test data every 1000 iterations
-data_test = AsyncHDF5DataLayer(name="data-test",source="train_test_data.txt",batch_size=250)
+data_test = AsyncHDF5DataLayer(name="data-test",source="train_test_data.txt",batch_size=128)
 accuracy_test = AccuracyLayer(name="accuracy-test",bottoms=[:pred, :label])
 net_test = Net("images-test", backend, [data_test, common_layers..., accuracy_test])
+
 add_coffee_break(solver, ValidationPerformance(net_test), every_n_iter=1000)
 
 solve(solver, net)
 
-data_val = AsyncHDF5DataLayer(name="data-val",source="validate_data.txt",batch_size=250,tops=[:data])
+data_val = AsyncHDF5DataLayer(name="data-val",source="validate_data.txt",batch_size=128,tops=[:data])
 output_val = HDF5OutputLayer(name="output-val",filename="validate_output.h5",force_overwrite=true,bottoms=[:pred],datasets=[:label])
 net_val = Net("images-val", backend, [data_val, common_layers..., output_val])
 forward(net_val)
--- project_data/convert.m
+++ project_data/convert.m
@@ -5,7 +5,7 @@ n = size(train_data,2);
 perm = randperm(n);
 train_data = train_data(:,perm);
 train_label = train_label(:,perm);
-s = 0.95*n;
+s = 0.9*n;
 
 hdf5write('train_double_train.h5','/data',double(train_data(:,1:s)),      '/label',double(train_label(:,1:s)));
 hdf5write('train_double_test.h5', '/data',double(train_data(:,(s+1):end)),'/label',double(train_label(:,(s+1):end)));
