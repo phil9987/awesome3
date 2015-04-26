f1bb1146ee55b5d73752debc2657dfbab8274596
Tim <tim@voodle.de>
2015-04-26 18:23:12 +0200

    Matlab classificatoin code with PCA

---
+++ project_data/pca_classify.m
@@ -0,0 +1,19 @@
+%convert;
+
+%COEFF_ALL = pca(train_data');
+COEFF = COEFF_ALL(:,1:200);
+labels = train_label';
+
+data_p = train_data' * COEFF;
+[trainInd,valInd,testInd] = dividerand(size(data_p,1),0.8,0,0.2);
+
+classifier = fitctree(data_p(trainInd,:), labels(trainInd,:));
+%BaggedEnsemble = TreeBagger(50,data_p(trainInd,:),labels(trainInd,:),'OOBPred','On');
+
+score_train = sum(predict(classifier,data_p(trainInd,:)) == labels(trainInd,:))/(0.8*size(data_p,1))
+score_test  = sum(predict(classifier,data_p(testInd,:)) == labels(testInd,:))/(0.2*size(data_p,1))
+
+data_val = validate_data' * COEFF;
+
+data_val_labels = predict(classifier,data_val);
+csvwrite('validate_matlab_output.txt',data_val_labels);
