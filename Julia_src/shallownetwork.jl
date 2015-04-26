d7a6fc7f6b16285ead362b7a552113b65668c283
Philip Junker <junkerp@student.ethz.ch>
2015-04-26 20:04:43 +0200

    shallownetwork implementation optimized, DropoutLayer added, staged lr-policy added submission id: 6014, grade 0.2732

--- Julia_src/shallownetwork.jl
+++ Julia_src/shallownetwork.jl
@@ -1,40 +1,50 @@
 using Mocha
 
-
-
-data = HDF5DataLayer(name="data", source="train_data.txt", batch_size=64, shuffle=true)
-fc1 = InnerProductLayer(name="ip1",output_dim=10,tops=[:ip1],bottoms=[:data],)
-loss = SoftmaxLossLayer(name="loss",bottoms=[:ip1,:label])
-
 backend = CPUBackend()
 init(backend)
 
-common_layers = [fc1]
+data = AsyncHDF5DataLayer(name="data", source="train_data.txt", batch_size=100, shuffle=true)
+
+common_layers = [InnerProductLayer(name="ip", output_dim=10,tops=[:pred],bottoms=[:ip2]),InnerProductLayer(name="ip2",output_dim=512,neuron=Neurons.Sigmoid(),tops=[:ip2],bottoms=[:data]), DropoutLayer(name="dropout",ratio=0.2,bottoms=[:data])]
+
+loss = SoftmaxLossLayer(name="loss",bottoms=[:pred,:label])
+#loss = SquareLossLayer(name="loss", bottoms=[:pred, :label])
+
 net = Net("images", backend, [data, common_layers..., loss])
 
 exp_dir= "snapshots"
-params = SolverParameters(max_iter=10000, regu_coef=0.0005,
+lr_policy = LRPolicy.Staged(
+  (60000, LRPolicy.Fixed(0.001)),
+  (5000, LRPolicy.Fixed(0.0001)),
+  (5000, LRPolicy.Fixed(0.00001)),
+)
+#lr_policy=LRPolicy.Inv(0.01,0.0001,0.75)
+params = SolverParameters(max_iter=5000, regu_coef=0.0004,
 	mom_policy=MomPolicy.Fixed(0.9),
-	lr_policy=LRPolicy.Inv(0.01,0.0001,0.75),
+	lr_policy=lr_policy,
 	load_from=exp_dir)
 solver=SGD(params)
 
-setup_coffe_lounge(solver, save_into="$exp_dir/output.jld", every_n_iter=1000)
+setup_coffee_lounge(solver, save_into="$exp_dir/output.jld", every_n_iter=250)
 
-# report training progress every 100 iterations
-add_coffee_break(solver,TrainingSummary(),every_n_iter=100)
-
-# save snapshots every 5000 iterations
+add_coffee_break(solver, TrainingSummary(), every_n_iter=250)
 add_coffee_break(solver, Snapshot(exp_dir), every_n_iter=5000)
 
-# show performance on test data every 1000 iterations
-data_test = AsyncHDF5DataLayer(name="test-data",source="validate_data.txt",batch_size=100)
-accuracy = AccuracyLayer(name="test-accuracy",bottoms=[:ip1, :label])
-test_net = Net("images-test", backend, [data_test, common_layers..., accuracy])
-add_coffee_break(solver, ValidationPerformance(test_net), every_n_iter=1000)
+data_test = AsyncHDF5DataLayer(name="data-test",source="train_test_data.txt",batch_size=100)
+accuracy_test = AccuracyLayer(name="accuracy-test",bottoms=[:pred, :label])
+net_test = Net("images-test", backend, [data_test, common_layers..., accuracy_test])
+
+add_coffee_break(solver, ValidationPerformance(net_test), every_n_iter=100)
 
 solve(solver, net)
 
+data_val = AsyncHDF5DataLayer(name="data-val",source="validate_data.txt",batch_size=10000,tops=[:data])
+output_val = HDF5OutputLayer(name="output-val",filename="validate_output.h5",force_overwrite=true,bottoms=[:pred],datasets=[:label])
+net_val = Net("images-val", backend, [data_val, common_layers..., output_val])
+
+forward(net_val)
+
 destroy(net)
-destroy(test_net)
+destroy(net_test)
+destroy(net_val)
 shutdown(backend)
--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import h5py
 import numpy as np
 
 def main():
-    train_file = h5py.File("./julia_src/project_data/train_label.h5", "r")
-    conv_file = h5py.File("./julia_src/project_data/train_conv.h5", "w")
+    validate = h5py.File("./julia_src/project_data/validate_double.h5")
+    train_file = h5py.File("./julia_src/validate_output.h5", "r")
+    for name in validate:
+        print name
     for name in train_file:
         print name
+
     #train_data = train_file['data'][:]
     train_label = train_file['label'][:]
     #print 'shape of train-data: ',train_data.shape
     print 'shape of train-label: ',train_label.shape
+    validate_data = validate['data'][:]
+    print 'shape of validate: ', validate_data.shape
     print train_label
     #print train_data
     #print train_label
