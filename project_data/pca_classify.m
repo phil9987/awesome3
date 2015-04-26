ed4f7ddbec27ff8df22dbdb3401772eda2aea029
Tim <tim@voodle.de>
2015-04-26 18:31:49 +0200

    WOrking on matlab classify

--- project_data/pca_classify.m
+++ project_data/pca_classify.m
@@ -8,7 +8,9 @@ data_p = train_data' * COEFF;
 [trainInd,valInd,testInd] = dividerand(size(data_p,1),0.8,0,0.2);
 
 classifier = fitctree(data_p(trainInd,:), labels(trainInd,:));
-%BaggedEnsemble = TreeBagger(50,data_p(trainInd,:),labels(trainInd,:),'OOBPred','On');
+%classifier = fitensemble(data_p(trainInd,:), labels(trainInd,:));
+
+%classifier = fitensemble(50,data_p(trainInd,:),labels(trainInd,:),'OOBPred','On','Method','classification');
 
 score_train = sum(predict(classifier,data_p(trainInd,:)) == labels(trainInd,:))/(0.8*size(data_p,1))
 score_test  = sum(predict(classifier,data_p(testInd,:)) == labels(testInd,:))/(0.2*size(data_p,1))
