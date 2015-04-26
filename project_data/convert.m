bd3259f74d7a36b3a7a134bf00324f0ea5bf7018
Tim <tim@voodle.de>
2015-04-26 16:23:25 +0200

    one_hot encoding in convert.m

--- project_data/convert.m
+++ project_data/convert.m
@@ -3,12 +3,13 @@ train_label = hdf5read('train.h5','/label');
 
 n = size(train_data,2);
 perm = randperm(n);
-train_data = train_data(:,perm);
-train_label = train_label(:,perm);
+train_data = double(train_data(:,perm));
+train_label = double(train_label(:,perm));
+train_label_one_hot = ind2vec(train_label+1);
 s = 0.9*n;
 
-hdf5write('train_double_train.h5','/data',double(train_data(:,1:s)),      '/label',double(train_label(:,1:s)));
-hdf5write('train_double_test.h5', '/data',double(train_data(:,(s+1):end)),'/label',double(train_label(:,(s+1):end)));
+hdf5write('train_double_train.h5','/data',train_data(:,1:s),      '/label',train_label(:,1:s));
+hdf5write('train_double_test.h5', '/data',train_data(:,(s+1):end),'/label',train_label(:,(s+1):end));
 
 validate_data = double(hdf5read('validate.h5','/data'));
 hdf5write('validate_double.h5','/data',validate_data);
